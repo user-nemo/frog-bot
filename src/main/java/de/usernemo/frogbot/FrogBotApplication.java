1858f23504077e4ef831dba9e7ce8f4255ebb0dc
user.nemo <lukas.schmiedgen@gmail.com>
2021-10-11 18:49:34 +0200

    initial version

---
+++ src/main/java/de/usernemo/frogbot/FrogBotApplication.java
@@ -0,0 +1,13 @@
+package de.usernemo.frogbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrogBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/usernemo/frogbot/FrogBotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.usernemo.frogbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
